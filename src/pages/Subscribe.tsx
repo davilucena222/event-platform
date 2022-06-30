2e3a80c1d39f5add6676b12b80354f44e64f0c48
Davi Luiz <davilucena222@gmail.com>
2022-06-29 22:56:06 -0300

    fixing sign up page bug

--- src/pages/Subscribe.tsx
+++ src/pages/Subscribe.tsx
@@ -2,6 +2,7 @@ import { useState, FormEvent } from "react";
 import { useNavigate } from "react-router-dom";
 import { Logo } from "../components/Logo";
 import { useCreateSubscriberMutation } from "../graphql/generated";
+import mockupImg from "../assets/code-mockup.png";
 
 export function Subscribe() {
   const navigate = useNavigate();
@@ -65,7 +66,7 @@ export function Subscribe() {
         </div>
       </div>
 
-      <img src="/src/assets/code-mockup.png" className="mt-10" alt="imagem da tela de inscrição" />
+      <img src={mockupImg} className="mt-10" alt="imagem da tela de inscrição" />
     </div>
   );
 }
